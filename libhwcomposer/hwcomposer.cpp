9abf258cd3cd95fd07933419571caa52e8d2c323
LorD ClockaN <davor@losinj.com>
2012-03-06 15:05:23 +0100

    CRT fix - Evervolv

--- libhwcomposer/hwcomposer.cpp
+++ libhwcomposer/hwcomposer.cpp
@@ -45,6 +45,7 @@
 #define ALIGN(x, align) (((x) + ((align)-1)) & ~((align)-1))
 #define LIKELY( exp )       (__builtin_expect( (exp) != 0, true  ))
 #define UNLIKELY( exp )     (__builtin_expect( (exp) != 0, false ))
+#define HWC_DEBUG 0
 
 enum HWCLayerType{
     HWC_SINGLE_VIDEO           = 0x1,
@@ -1250,6 +1251,21 @@ static int hwc_set(hwc_composer_device_t *dev,
     if(!ctx) {
         LOGE("hwc_set invalid context");
         return -1;
+    }
+
+    if (dpy == NULL && sur == NULL && list == NULL) {
+        // special case: the screen is off, there is nothing to do.
+        LOGD_IF(HWC_DEBUG,"hwc_set screen is off");
+        return 0;
+    } else if (!list) {
+        // allow hwc_set to partially execute here, hack for screen off animation
+        LOGD_IF(HWC_DEBUG,"hwc_set invalid list: attempting hack");
+        EGLBoolean sucess = eglSwapBuffers((EGLDisplay)dpy, (EGLSurface)sur);
+        if (!sucess) {
+            LOGE("hwc_set invalid list and eglSwapBuffers() failed");
+            return HWC_EGL_ERROR;
+        }
+        return 0;
     }
 
     private_hwc_module_t* hwcModule = reinterpret_cast<private_hwc_module_t*>(
