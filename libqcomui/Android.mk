21b37a6e97e5d5d73f662248729d1bfd2cf6778c
Jeykumar Sankaran <jsanka@codeaurora.org>
2012-03-21 18:05:54 -0700

    libqcomUI: Add utils folder to libqcomUI
    -Introduce utils folder in qcomUI lib for all Qcom
     implemented utility classes.
    -Move FPS calculation profiler class into utils
     folder.
    
    CRs-Fixed: 342060
    (cherry picked from commit e9469022741642cfd8483839de96e809f4d94f1e)
    
    Change-Id: Ie0af0c0cc10b5304814b4f26aed581ea93ce7f5f

--- libqcomui/Android.mk
+++ libqcomui/Android.mk
@@ -2,7 +2,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-        qcom_ui.cpp
+        qcom_ui.cpp \
+        utils/profiler.cpp
 
 LOCAL_SHARED_LIBRARIES := \
         libutils \
