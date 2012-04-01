78a79ace6e219671abc0e75208faadff6ce3f6b8
Rob Beane <robbeane@gmail.com>
2012-03-31 23:16:27 -0400

    libgralloc: fix typo in Android.mk

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -59,7 +59,7 @@ include $(CLEAR_VARS)
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_SHARED_LIBRARIES := liblog libcutils libutils
-LOCAL_SRC_FILES +=  ashmemalloc.cpp \
+LOCAL_SRC_FILES :=  ashmemalloc.cpp \
                     pmemalloc.cpp \
                     pmem_bestfit_alloc.cpp \
                     alloc_controller.cpp
