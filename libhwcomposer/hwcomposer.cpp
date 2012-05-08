0ab0b444cd4b8b72bc04709a22e926a8d6c27ceb
Sushil Chauhan <sushilchauhan@codeaurora.org>
2012-05-07 18:38:59 -0700

    libhwcomposer: Set bypass transition flag before invalidate()
    Set flag to ensure HWComposer::invalidate() does not draw with
    bypass still enabled. Also, log comp bypass transition due to
    idle-timeout.
    
    Change-Id: I3343d092a92c9677d0b9d50ced094c96d229474a
    (cherry picked from commit f27aebd66249d165c0a3342abf4064a5dc350a6d)

--- libhwcomposer/hwcomposer.cpp
+++ libhwcomposer/hwcomposer.cpp
@@ -168,6 +168,7 @@ static inline int max(const int& a, const int& b) {
 }
 #ifdef COMPOSITION_BYPASS
 static void timeout_handler(void *udata) {
+    LOGD("Comp bypass timeout_handler...");
     struct hwc_context_t* ctx = (struct hwc_context_t*)(udata);
 
     if(!ctx) {
@@ -182,8 +183,9 @@ static void timeout_handler(void *udata) {
         return;
     }
     /* Trigger SF to redraw the current frame */
-    proc->invalidate(proc);
     ctx->idleTimeOut = true;
+    proc->invalidate(proc);
+    LOGD("Comp bypass timeout_handler...Done");
 }
 
 void setLayerbypassIndex(hwc_layer_t* layer, const int bypass_index)
