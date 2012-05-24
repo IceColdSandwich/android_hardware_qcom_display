ddd36ee338768793b758a0f05c61f8f2a00f5277
Saurabh Shah <saurshah@codeaurora.org>
2012-05-24 11:38:11 -0700

    Fix incorrect video overlay status update.
    Status should be set to PREPARE_TO_CLOSE only if its OPEN.
    Devices which rely on the status being CLOSED, if overlay
    is unavailable, can crash.
    
    (cherry picked from commit a6ee9945dedde6f2d119f0bdd8724fed20781f0e)
    
    Conflicts:
    
    	libhwcomposer/badger/hwcomposer.cpp
    
    Change-Id: Iddf0ba1cc142e02fcf906d3b31f8f44a65def316

--- libhwcomposer/hwcomposer.cpp
+++ libhwcomposer/hwcomposer.cpp
@@ -1631,7 +1631,8 @@ static int hwc_set(hwc_composer_device_t *dev,
             }
         }
     } else {
-        ctx->hwcOverlayStatus =  HWC_OVERLAY_PREPARE_TO_CLOSE;
+            if (ctx->hwcOverlayStatus == HWC_OVERLAY_OPEN)
+                ctx->hwcOverlayStatus =  HWC_OVERLAY_PREPARE_TO_CLOSE;
     }
     
 
